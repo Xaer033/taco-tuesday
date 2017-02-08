184c923ade994f55df2d473adb42d7e3247dcb44
Julian Williams <jdfrag@gmail.com>
2017-02-08 01:16:33 -0800

    Put card drag and drop functionality into its own class

---
+++ UnityProject/Assets/Src/Game/Views/CardDragDropController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class CardDragDropController
+{
+    const int kMaxSmoothFrame = 7;
+    const float kDragScale = 1.2f;
+
+    private Transform _target;
+    private Transform _slot;
+    private Transform _dragLayer;
+
+    private Vector3 _inputOffset;
+
+    private Vector2[] _averageDelta = new Vector2[kMaxSmoothFrame];
+    private int _deltaIndex = 0;
+    private bool _isDragging = false;
+    private Vector2 _dragDelta;
+
+    private Vector3 _originalScale;
+
+    private Tween _scaleTween;
+    private Tween _resetTween;
+    private float _deltaTime;
+
+
+    public CardDragDropController(
+        Transform targetCard, 
+        Transform slot, 
+        Transform dragLayer)
+    {
+        _target = targetCard;
+        _slot = slot;
+        _dragLayer = dragLayer;
+
+        _originalScale = _target.localScale;
+
+        for (int i = 0; i < kMaxSmoothFrame; ++i)
+        {
+            _averageDelta[i] = Vector2.zero;
+        }
+    }
+	
+	// Update is called once per frame
+	public void Step (float deltaTime)
+    {
+        if (_isDragging)
+        {
+            _updateDragRotation(deltaTime);
+        }
+    }
+
+    public void OnDragBegin(PointerEventData eventData)
+    {
+        if (_resetTween != null) { _resetTween.Kill(true); }
+
+        if (_scaleTween != null) { _scaleTween.Kill(); }
+        _scaleTween = _target.DOScale(_originalScale * kDragScale, 0.82f);
+        _scaleTween.SetEase(Ease.OutBack);
+        _scaleTween.OnComplete(() => _scaleTween = null);
+        
+
+        Vector3 mPos = Input.mousePosition;
+        mPos.z = GameManager.Get().guiCanvas.planeDistance;
+        _inputOffset = _target.position - Camera.main.ScreenToWorldPoint(mPos);
+
+        _target.SetParent(_dragLayer);
+        _isDragging = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        const float kScaleFactor = 2.0f;
+
+        Vector3 mPos = Input.mousePosition;
+        mPos.z = GameManager.Get().guiCanvas.planeDistance;
+        mPos = Camera.main.ScreenToWorldPoint(mPos) + _inputOffset;
+        _target.position =mPos + (Camera.main.transform.position - mPos) * 0.1f;
+       
+        _dragDelta = -eventData.delta * kScaleFactor;
+    }
+
+    public void OnDragEnd(PointerEventData eventData)
+    {
+        const float kTweenDuration = 0.523f;
+
+        _isDragging = false;
+
+        for (int i = 0; i < kMaxSmoothFrame; ++i)
+        {
+            _averageDelta[i] = Vector2.zero;
+        }
+        _resetTween = _target.DOMove(_slot.position, kTweenDuration);
+        _resetTween.SetEase(Ease.OutCubic);
+        _resetTween.OnComplete(() =>
+        {
+            _resetTween = null;
+            _target.SetParent(_slot);
+        });
+
+        _target.DOLocalRotateQuaternion(Quaternion.identity, kTweenDuration * 0.6f);
+
+        if (_scaleTween != null) { _scaleTween.Kill(true); }
+        _scaleTween = _target.DOScale(_originalScale, kTweenDuration * 0.756f);
+        _scaleTween.OnComplete(() => _scaleTween = null);
+    }
+
+    private void _updateDragRotation(float deltaTime)
+    {
+        int index = _deltaIndex++ % kMaxSmoothFrame;
+        _averageDelta[index] = _dragDelta;
+
+        Vector2 smoothDelta = Vector2.zero;
+        for (int i = 0; i < kMaxSmoothFrame; ++i)
+        {
+            smoothDelta += _averageDelta[i];
+        }
+
+        smoothDelta /= kMaxSmoothFrame;
+        smoothDelta = Vector2.Max(smoothDelta, -Vector2.one * 15.0f);
+        smoothDelta = Vector2.Min(smoothDelta, Vector2.one * 15.0f);
+
+        _target.localRotation = Quaternion.Slerp(
+            Quaternion.Euler(-smoothDelta.y, smoothDelta.x, 0), 
+            _target.localRotation, 
+            deltaTime
+        );
+
+        _dragDelta = Vector2.zero;
+    }
+}
+
--- UnityProject/Assets/Src/Game/Views/IngredientCardView.cs
+++ UnityProject/Assets/Src/Game/Views/IngredientCardView.cs
@@ -9,53 +9,31 @@ using DG.Tweening;
 
 [System.Serializable]
 [RequireComponent(typeof(EventTrigger))]
-public sealed class IngredientCardView : BaseCardView
+public sealed class IngredientCardView : 
+    BaseCardView, 
+    IBeginDragHandler, 
+    IEndDragHandler, 
+    IDragHandler
 {
-    const int kMaxSmoothFrame = 5;
-    const float kDragScale = 1.2f;
-
     private EventTrigger _eventTrigger;
-    private Vector3 _inputOffset;
-    private RectTransform _rectXform;
-    private Tween _resetTween;
 
-    private Vector3 _prevDragPosition;
-    private Vector2[] _averageDelta = new Vector2[kMaxSmoothFrame];
-    private int _deltaIndex = 0;
-    private bool _isDragging = false;
-    private Vector2 _dragDelta;
-
-    private Vector3 _originalScale;
-    private Tween _scaleTween;
+    private CardDragDropController _dragDropController;
 
     public PlayerHandView handView { get; set; }
     public Transform handSlot { get; set; }
     public Transform dragLayer { get; set; }
-
-    void Awake()
-    {
-        _rectXform = (RectTransform)transform;
-        _setup();
-
-        for(int i = 0; i < kMaxSmoothFrame; ++i)
-        {
-            _averageDelta[i] = Vector2.zero;
-        }
-    }
+    
 
     void Start()
     {
+        _setup();
         OnIntroTransitionFinished();
     }
     
     public override void Update()
     {
         base.Update();
-
-        if(_isDragging)
-        {
-            _updateDragRotation();
-        }
+        _dragDropController.Step(Time.deltaTime);
     }
 
     protected override void OnViewUpdate()
@@ -79,86 +57,22 @@ public sealed class IngredientCardView : BaseCardView
 
     private void _setup()
     {
-        _eventTrigger = GetComponent<EventTrigger>();
-        _originalScale = transform.localScale;
+        _dragDropController = new CardDragDropController(transform, handSlot, dragLayer);
     }
 
-    public void onCardBeginDrag(BaseEventData e)
+    public void OnBeginDrag(PointerEventData e)
     {
-        PointerEventData eventData = (PointerEventData)e;
-
-        if(_resetTween != null) { _resetTween.Kill(true); }
-
-        if (_scaleTween != null) { _scaleTween.Kill(); }
-        _scaleTween = transform.DOScale(_originalScale * kDragScale, 0.82f);
-        _scaleTween.SetEase(Ease.OutBack);
-        _scaleTween.OnComplete(() => _scaleTween = null);
-
-        transform.SetParent(dragLayer);
-
-        Vector3 mPos = Input.mousePosition;
-        mPos.z = GameManager.Get().guiCanvas.planeDistance;
-        _inputOffset = _rectXform.position - Camera.main.ScreenToWorldPoint(mPos);
-       
-        handView.canvasGroup.blocksRaycasts = false;
-        _isDragging = true;
+        _dragDropController.OnDragBegin(e);
     }
 
-    public void onCardDrag(BaseEventData e)
+    public void OnDrag(PointerEventData e)
     {
-        PointerEventData eventData = (PointerEventData)e;
-        Vector3 mPos = Input.mousePosition;
-        mPos.z = GameManager.Get().guiCanvas.planeDistance;
-        _rectXform.position = Camera.main.ScreenToWorldPoint(mPos) + _inputOffset;
-        _rectXform.position = _rectXform.position + (Camera.main.transform.position - _rectXform.position) * 0.1f;
-        const float kScaleFactor = 2.0f;
-
-        _dragDelta = -eventData.delta * kScaleFactor;
+        _dragDropController.OnDrag(e);
     }
-
-    private void _updateDragRotation()
+    
+    public void OnEndDrag(PointerEventData e)
     {
-        int index = _deltaIndex++ % kMaxSmoothFrame;
-        _averageDelta[index] = _dragDelta;
-
-        Vector2 smoothDelta = Vector2.zero;
-        for(int i = 0; i < kMaxSmoothFrame; ++i)
-        {
-            smoothDelta += _averageDelta[i];
-        }
-
-        smoothDelta /= kMaxSmoothFrame;
-        smoothDelta = Vector2.Max(smoothDelta, -Vector2.one * 15.0f);
-        smoothDelta = Vector2.Min(smoothDelta, Vector2.one * 15.0f);
-        transform.localRotation = Quaternion.Slerp(Quaternion.Euler(-smoothDelta.y, smoothDelta.x, 0), transform.localRotation, Time.deltaTime);
-        _dragDelta = Vector2.zero;
-    }
-
-    public void onCardEndDrag(BaseEventData e)
-    {
-        PointerEventData eventData = (PointerEventData)e;
         handView.canvasGroup.blocksRaycasts = true;
-        _isDragging = false;
-
-        for (int i = 0; i < kMaxSmoothFrame; ++i)
-        {
-            _averageDelta[i] = Vector2.zero;
-        }
-
-        const float kTweenDuration = 0.523f;
-
-        _resetTween = transform.DOMove(handSlot.position, kTweenDuration);
-        _resetTween.SetEase(Ease.OutCubic);
-        _resetTween.OnComplete(() =>
-        {
-            _resetTween = null;
-            transform.SetParent(handSlot);
-        });
-
-        transform.DOLocalRotateQuaternion(Quaternion.identity, kTweenDuration * 0.6f);
-        
-        if(_scaleTween != null) { _scaleTween.Kill(true); }
-        _scaleTween = transform.DOScale(_originalScale, kTweenDuration * 0.756f);
-        _scaleTween.OnComplete(() => _scaleTween = null);
+        _dragDropController.OnDragEnd(e);
     }
 }
